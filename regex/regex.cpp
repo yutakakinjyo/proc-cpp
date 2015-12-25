a413b5c3a5e9538c18585a40edb48ef71ab16a7f
yutakakinjyo <yutakakinjyo@gmail.com>
2015-12-25 12:17:21 +0900

    regex search g++ -lboost_regex regex.cpp

--- regex/regex.cpp
+++ regex/regex.cpp
@@ -1,3 +1,13 @@
+#include <boost/regex.hpp>
+
 int main(void) {
+  std::string str = "hoge";
+  std::string::const_iterator begin = str.begin();
+  std::string::const_iterator end = str.end();
+  boost::smatch matches;
+  boost::regex pattern("hoge");
+  while (boost::regex_search(begin, end, matches, pattern)) {
+  }
+    
   return 0;
 }
