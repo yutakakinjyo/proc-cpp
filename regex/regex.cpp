3adf340317594e7f47a680d2b18dca7bf493440b
yutakakinjyo <yutakakinjyo@gmail.com>
2015-12-25 12:25:40 +0900

    imple

--- regex/regex.cpp
+++ regex/regex.cpp
@@ -1,12 +1,15 @@
+#include <iostream>
 #include <boost/regex.hpp>
 
 int main(void) {
-  std::string str = "hoge";
+  std::string str = "hogehuga";
   std::string::const_iterator begin = str.begin();
   std::string::const_iterator end = str.end();
   boost::smatch matches;
   boost::regex pattern("hoge");
   while (boost::regex_search(begin, end, matches, pattern)) {
+    begin = matches[0].second;
+    std::cout << matches[0] << std::endl;
   }
     
   return 0;
