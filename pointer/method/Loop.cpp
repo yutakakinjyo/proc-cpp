9e195c244cfb4de96f7be006b00fa7abb372da81
yutakakinjyo <yutakakinjyo@gmail.com>
2015-12-26 15:21:08 +0900

    get method by arg

--- pointer/method/Loop.cpp
+++ pointer/method/Loop.cpp
@@ -9,14 +9,14 @@ int loop(int (*calc)(int, int)) {
   return sum;
 }
 
-std::string regex_loop(std::string str) {
+std::string regex_loop(std::string str, std::string (*regex)(std::string)) {
   std::string::const_iterator begin = str.begin();
   std::string::const_iterator end = str.end();
   boost::smatch matches;
   boost::regex pattern(".+");
   while (boost::regex_search(begin, end, matches, pattern)) {
     std::string match = matches.str();
-    str.replace(begin, begin+match.size(), "hoge");
+    str.replace(begin, begin+match.size(), regex(match));
     begin = matches[0].second;
   }
   return str;
--- pointer/method/method.cpp
+++ pointer/method/method.cpp
@@ -9,9 +9,13 @@ int sub(int a, int b) {
   return a - b;
 }
 
+std::string regex(std::string match) {
+  return "hoge";
+}
+
 int main(void) {
   std::cout << loop(add) << std::endl;
   std::cout << loop(sub) << std::endl;
-  std::cout << regex_loop("hoge") << std::endl;
+  std::cout << regex_loop("hoge", regex) << std::endl;
   return 0;
 }
