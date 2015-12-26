c9253805df3c7697d4d2573aaf107fb280166843
yutakakinjyo <yutakakinjyo@gmail.com>
2015-12-26 13:00:55 +0900

    regex loop

--- pointer/method/Loop.cpp
+++ pointer/method/Loop.cpp
@@ -1,5 +1,23 @@
 #include "Loop.h"
 
 int loop(int (*calc)(int, int)) {
-  return calc(1,2);
+  int sum = 0;
+  for(int i = 0; i < 10; i++) {
+    sum += calc(1,2);
+  }
+  
+  return sum;
+}
+
+std::string regex_loop(std::string str) {
+  std::string::const_iterator begin = str.begin();
+  std::string::const_iterator end = str.end();
+  boost::smatch matches;
+  boost::regex pattern(".+");
+  while (boost::regex_search(begin, end, matches, pattern)) {
+    std::string match = matches.str();
+    str.replace(begin, begin+match.size(), "hoge");
+    begin = matches[0].second;
+  }
+  return str;
 }
--- pointer/method/method.cpp
+++ pointer/method/method.cpp
@@ -9,9 +9,9 @@ int sub(int a, int b) {
   return a - b;
 }
 
-
 int main(void) {
   std::cout << loop(add) << std::endl;
   std::cout << loop(sub) << std::endl;
+  std::cout << regex_loop("hoge") << std::endl;
   return 0;
 }
