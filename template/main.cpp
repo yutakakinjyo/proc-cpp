f37d276355cbc024621a6cfb1cf4fcf54c48d957
yutakakinjyo <yutakakinjyo@gmail.com>
2016-01-06 20:47:53 +0900

    template

--- template/main.cpp
+++ template/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+template<typename T>
+T add(T a, T b) {
+  return a + b;
+};
+
+
 int main(void) {
-  std::cout << "hoge" << std::endl;
+  std::string str = add<std::string>("a", "b");
+  std::cout << str << std::endl;
+  
   return 0;
 }
