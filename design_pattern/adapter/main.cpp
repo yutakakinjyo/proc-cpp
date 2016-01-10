2038d28da732b09f32584a45347b3262d6f8e0cc
yutakakinjyo <yutakakinjyo@gmail.com>
2016-01-10 19:35:13 +0900

    use instance for argument of Constractor

--- design_pattern/adapter/main.cpp
+++ design_pattern/adapter/main.cpp
@@ -3,9 +3,9 @@
 #include "product_adapter.h"
 
 int main(void) {
-  Product product(10);
-  product.getCost();
-  ProductAdapter product_adapter(30);
+  Product *product = new Product(10);
+  product->getCost();
+  ProductAdapter product_adapter(product);
   product_adapter.getPrice();
   return 0;
 }
