4faffb5365dd8d5f1eeb751622a6e5440f03a364
yutakakinjyo <yutakakinjyo@gmail.com>
2016-01-15 19:32:42 +0900

    get singleton instance

--- design_pattern/singleton/main.cpp
+++ design_pattern/singleton/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "singleton.h"
 
 int main(void) {
-  std::cout << "hoge" << std::endl;
+
+  // refarence. it can receive refarence or value type.
+  Singleton &singleton = Singleton::getInstance();
+  std::cout << singleton.i << std::endl;
+  singleton.i = 1;
+
+  Singleton singleton2 = Singleton::getInstance();
+  std::cout << singleton2.i << std::endl;
 }
