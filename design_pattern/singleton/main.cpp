58f45a83c3a760c7eeb4b6b8e563020e94e4d15b
yutakakinjyo <yutakakinjyo@gmail.com>
2016-01-15 19:37:55 +0900

    add comment

--- design_pattern/singleton/main.cpp
+++ design_pattern/singleton/main.cpp
@@ -3,11 +3,14 @@
 
 int main(void) {
 
-  // refarence. it can receive refarence or value type.
-  Singleton &singleton = Singleton::getInstance();
+  // refarence. it can receive reference or value type.
+
+  // receive referecne type.
+  Singleton& singleton = Singleton::getInstance();
   std::cout << singleton.i << std::endl;
   singleton.i = 1;
 
+  // receive value type.
   Singleton singleton2 = Singleton::getInstance();
   std::cout << singleton2.i << std::endl;
 }
